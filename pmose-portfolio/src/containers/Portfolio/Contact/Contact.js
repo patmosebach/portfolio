76d7e8b5f2cf64819c5968a7a17d93808134594d
patrick.mosebach <patrick.mosebach@ticketmaster.com>
2020-11-22 10:59:47 -0600

    Email Send

--- pmose-portfolio/src/containers/Portfolio/Contact/Contact.js
+++ pmose-portfolio/src/containers/Portfolio/Contact/Contact.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import classes from './Contact.module.css';
 import Button from 'react-bootstrap/Button';
+import emailjs from 'emailjs-com';
+
 
 class Contact extends Component  {
 
-    sentEmail = () => {
-        
-    };
+    constructor(){
+        super();
+
+        emailjs.init("user_NCQi93r4LPgHD8dtuU90d");
+    }
+
+    state = {
+        email: '',
+        subject: '',
+        message: ''
+    }
+
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({[name]: value})
+    }
+
+    handleSend = (event) => {
+        event.preventDefault();
+
+        emailjs.sendForm('service_5lafehf', 'template_ucqiqz7', event.target, 'user_NCQi93r4LPgHD8dtuU90d')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
 
 
     render(){
@@ -17,17 +46,22 @@ class Contact extends Component  {
                 </h1>
     
                 <ul className={classes.ContactList}>
-                    <li>Web application Development</li><br/>
-                    <li>Mobile application Development</li><br/>
-                    <li>Freelance development</li><br/>
-                    <li>Website updates</li><br/>
-                    <li>Database maintanence</li><br/>
+                    <li>pat.mosebach@gmail.com</li><br/>
+                    <li>Employment Opportunites</li><br/>
+                    <li>Contracts</li><br/>
+                    <li>Freelance gigs</li><br/>
+                    <li>Website updates</li>
                 </ul>
-    
-                <input placeholder={'Email'} className={classes.InputMargin}/><br/>
-                <input placeholder={'Subject'} className={classes.InputMargin}/><br/>
-                <textarea placeholder={'Message'} className={classes.InputMargin}/><br/>
-                <Button>Send</Button>
+                <ul className={classes.ContactList}>
+                    <li>Database maintanence</li><br/>  
+                </ul>
+                <form name="contactForm" onSubmit={this.handleSend} className={classes.EmailForm}>
+                    <input name="email" placeholder={'Email'} className={classes.InputMargin}/><br/>
+                    <input name="subject" placeholder={'Subject'} className={classes.InputMargin}/><br/>
+                    <textarea name="message"  placeholder={'Message'} className={classes.MessageBody}/><br/>
+                    <Button type="submit" value="Submit">Send</Button>
+                </form>
+
             </div>
         );
     }
