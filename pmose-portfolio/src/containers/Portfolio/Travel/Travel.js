a8db50be13ee0004f18dbb029321790e983125a6
patrick.mosebach <patrick.mosebach@ticketmaster.com>
2020-12-04 15:01:16 -0600

    Travel Cleanup

--- pmose-portfolio/src/containers/Portfolio/Travel/Travel.js
+++ pmose-portfolio/src/containers/Portfolio/Travel/Travel.js
@@ -14,51 +14,53 @@ import windRiver from '../../../assets/images/WindRiver.png';
 
 const PROJECTS = [
     {
-        title: 'Canyonlands 2017',
-        image: canyonlandsTrail,
-        black: true,
-        description: 'The objective of the mars rover project was to program a robot to traverse a dangerous environment autonomously. The iRobot Create that we wercessor implementation.'
-    },
-    {
-        title: 'Canyonlands 2017',
-        image: canyonlands,
-        black: true,
-        description: 'During this project I learned how to program with VHDL, avoid pipeline hazards, how to effectively write MIPS 32 assembly and implement various CPU components (i.e. ALU, Control Unit). By implementing this processor in VHDL we were able to test weather we could run assembly code correctly in simulations. Using wave forms of signals and virtual instantiates of the registers and a computer\'s memory we could watch the processor execute assembly and C code step-by-step. Learning how a pipeline processor works is an important topic because it is far more efficient than just a single cycle. The purpose of this project wa',
-    },
-    {
-        title: 'Sawtooth 2020',
-        image: sawtoothTrail,
-        black: false,
-        description: 'While taking embedded systems 2 we designed a digital camera that displayed via HDMI to a monitor. The hardware portion of this project consisted of connecting a vdma, video timing clock and video out modules in order to set up the correct connections. While writing the software that programmed the system we learned about rgb to YCrCb conversion as well as outputting 4:2:2 format YCrCb. We also learned about how frame buffers work and in what ways the vdma can allow us to access that memory efficiently.',
-    },    {
-        title: 'Sawtooth 2020',
-        image: sawtooth,
-        black: true,
-        description: 'The objective of the mars rover project was to program a robot to traverse a dangerous environment autonomously. The iRobot Create that we were supplied with was customized with an arduino Atmel ATmega 128 microprocessor. By compartmental implementing sensors that the microprocessor could access, we slowly increased the ability of the robot. When we had full control of the robot was when we started writing software to control it without looking where we were going. Using the sensors we were able to navigate the course successfully and eventually have the robot reach the checkpoint autonomously. While doing the project'
-    },
-    {
-        title: 'Grand Tetons 2016',
+        title: 'Grand Teton National Forest, WY',
         image: tetonsTrail,
         black: true,
-        description: 'During this project I learned how to program with VHDL, avoid pipeline hazards, how to effectively write MIPS 32 assembly and implement various CPU components (i.e. ALU, Control Unit). By implementing this processor in VHDL we were able to test weather we could run assembly code correctly in simulations. Using wave forms of signals and virtual instantiates of the registers and a computer\'s memory we could watch the processor execute assembly and C code step-by-step. Learning how a pipeline processor works is an important topic because it is far more efficient than just a single cycle. The purpose of this pro',
+        description: 'The Teton Crest Trail is one of the higher rated trails in the US. After starting at the Death Canyon trailhead south of the Grand Teton, we spent a night below the shelf and one on the shelf. Working our way north to spend one night in the alaska basin at lake sunset lake where we hunkered down for a thunderstorm. we Continued through the south and north cascade canyon trails with a stark view of the teton as we headed north away from the Tetons twords our finish. We rounded a corner east through paintbrush canyon to our exit at string lake.',
     },
     {
-        title: 'Grand Tetons 2016',
+        title: 'Grand Teton National Forest, WY',
         image: tetons,
         black: false,
-        description: 'While taking embedded systems 2 we designed a digital camera that displayed via HDMI to a monitor. The hardware portion of this project consisted of connecting a vdma, video timing clock and video out modules in order to set up the correct connections. While writing the software that programmed the system we learned about rgb to YCrCb conversion as well as outputting 4:2:2 format YCrCb. We also learned about how frame buffers work and in what ways the vdma can allow us to access that memory efficiently.',
-    },    {
-        title: 'Wind River 2019',
-        image: windRiverTrail,
-        black: true,
-        description: 'The objective of the mars rover project was to program a robot to traverse a dangerous environment autonomously. The iRobot Create that we were supplied with was customized with an arduino Atmel ATmega 128 microprocessor. By compartmental implementing sensors that the microprocessor could access, we slowly increased the ability of the robot. When we had full control of the robot was when we started writing software to control it without looking where we were going. Using the sensors we were able to navigate the course successfully and eventually have the robot reach the checkpoint autonomously. While doin'
+        description: 'The Teton Crest Trail is one of the higher rated trails in the US. After starting at the Death Canyon trailhead south of the Grand Teton, we spent a night below the shelf and one on the shelf. Working our way north to spend one night in the alaska basin at lake sunset lake where we hunkered down for a thunderstorm. we Continued through the south and north cascade canyon trails with a stark view of the teton as we headed north away from the Tetons twords our finish. We rounded a corner east through paintbrush canyon to our exit at string lake.',
     },
     {
-        title: 'Wind River 2019',
+        title: 'Canyonlands National Park, UT',
+        image: canyonlandsTrail,
+        black: true,
+        description: 'Canyonlands is around 45 minutes south of town of Moab and Arches National Park. Second in popularity to Arches, it offers alien mars-like landscape and if you would like some time alone this is the place to go. Starting at Squaw Canyon Trailhead we ended up off trail lost the very first day as this was just our second backpacking trip. We hiked through Lost Canyon and camped beside a dried up river. Then we made our way to the Needles, Canyonlands prized landmark (pictured). The following day we day hiked southwest through Chesler Park and took refuge in the Joint Crack through the afternoon heat. The final day we exited past Elephant Hill to the Elephant Hill Trailhead.'
+    },
+    {
+        title: 'Canyonlands National Park, UT',
+        image: canyonlands,
+        black: true,
+        description: 'Canyonlands is around 45 minutes south of town of Moab and Arches National Park. Second in popularity to Arches, it offers alien mars-like landscape and if you would like some time alone this is the place to go. Starting at Squaw Canyon Trailhead we ended up off trail lost the very first day as this was just our second backpacking trip. We hiked through Lost Canyon and camped beside a dried up river. Then we made our way to the Needles, Canyonlands prized landmark (pictured). The following day we day hiked southwest through Chesler Park and took refuge in the Joint Crack through the afternoon heat. The final day we exited past Elephant Hill to the Elephant Hill Trailhead.',
+    },
+    {
+        title: 'Wind River Mountain Range, WY',
+        image: windRiverTrail,
+        black: true,
+        description: 'With roughly 40 peaks over 13,000 feet, the Wind River Range is a destination to behold. Southeast of Grand Tetons National Forest the Titcomb Basin Trail is a highly trafficed backpacking destination lined with glacial lakes, snowy peaks and wildflower fields. On our way to the Titcomb lakes we passed various prestine lakes including: Hobbs Lake, Seneca Lake and Island Lake. West of the Titcomb Lakes we attempted a summit of Fremont Peak to the Northeast. Endeing up only making it to the ridge to the summit where we caught an awesome view of the Titcomb Lakes to the west (pictured) as well as Harrower Peak to the east.'
+    },
+    {
+        title: 'Wind River Mountain Range, WY',
         image: windRiver,
         black: true,
-        description: 'During this project I learned how to program with VHDL, avoid pipeline hazards, how to effectively write MIPS 32 assembly and implement various CPU components (i.e. ALU, Control Unit). By implementing this processor in VHDL we were able to test weather we could run assembly code correctly in simulations. Using wave forms of signals and virtual instantiates of the registers and a computer\'s memory we could watch the processor execute assembly and C code step-by-step. Learning how a pipeline processor works is an important topic because it is far more efficient than just a single cycle. The purpose of this project w.',
+        description: 'With roughly 40 peaks over 13,000 feet, the Wind River Range is a destination to behold. Southeast of Grand Tetons National Forest the Titcomb Basin Trail is a highly trafficed backpacking destination lined with glacial lakes, snowy peaks and wildflower fields. On our way to the Titcomb lakes we passed various prestine lakes including: Hobbs Lake, Seneca Lake and Island Lake. West of the Titcomb Lakes we attempted a summit of Fremont Peak to the Northeast. Endeing up only making it to the ridge to the summit where we caught an awesome view of the Titcomb Lakes to the west (pictured) as well as Harrower Peak to the east.',
+    },
+    {
+        title: 'Sawtooth National Forest, ID',
+        image: sawtoothTrail,
+        black: false,
+        description: 'Idaho might not seem like the premium destination, but it is truely a hidden gem. We made our way around the Alice-Toxaway Loop passing crystal glacial lakes including: Alice Lake (pictured), Alice Lake, Farley and Mirror. Starting at Petite Lake Trailhead our farthest Lake was Edna where we woke up in the early hour to Summit Glens Peak Passing even more glacial lakes on the way. Our views were lessened by the increasing smog from the various wildfires in Oregon and California. Never the less we found ourselves jumping in lakes and enjoying some of the best views Ive seen to date. ',
+    },    {
+        title: 'Sawtooth National Forest, ID',
+        image: sawtooth,
+        black: true,
+        description: 'Idaho might not seem like the premium destination, but it is truely a hidden gem. We made our way around the Alice-Toxaway Loop passing crystal glacial lakes including: Alice Lake (pictured), Alice Lake, Farley and Mirror. Starting at Petite Lake Trailhead our farthest Lake was Edna where we woke up in the early hour to Summit Glens Peak Passing even more glacial lakes on the way. Our views were lessened by the increasing smog from the various wildfires in Oregon and California. Never the less we found ourselves jumping in lakes and enjoying some of the best views Ive seen to date. '
     }
+
 ];
 
 class Travel extends Component {
