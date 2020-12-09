b2f755c95dd9aa85f78b541528aad28496bad69a
patrick.mosebach <patrick.mosebach@ticketmaster.com>
2020-12-09 17:28:13 -0600

    Mobile Intro cleaned

--- pmose-portfolio/src/containers/Portfolio/Intro/Intro.js
+++ pmose-portfolio/src/containers/Portfolio/Intro/Intro.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Adj from '../../../hoc/Adj/Adj';
 import classes from './Intro.module.css';
 import Logo from '../../../components/UI/Logo/Logo';
+import Image from 'react-bootstrap/Image';
 
 import { ReactComponent as TmLogo } from '../../../assets/images/ticketmaster.svg';
 import { ReactComponent as IsuLogo } from '../../../assets/images/isu.svg'
+import selfPic from '../../../assets/images/portfolio.png';
+import { createBootstrapComponent } from 'react-bootstrap/esm/ThemeProvider';
 
 const intro = (props) => {
 
@@ -15,6 +18,7 @@ const intro = (props) => {
 
     return(
         <Adj>
+            <Image className={ classes.SelfPicture }fluid roundedCircle src={selfPic}></Image>
             <div className={classes.Intro}>
                 Patrick Mosebach is a <span>full stack engineer</span> with a passion for making scalable solutions.
             </div>
@@ -29,7 +33,7 @@ const intro = (props) => {
                 <Logo link={isuLink}>
                     <IsuLogo className={classes.IsuLogo}></IsuLogo>
                 </Logo>
-                <div>2013  -  2017</div>
+                <div className={classes.BottomDate}>2013  -  2017</div>
             </div>
         </Adj>
     );
